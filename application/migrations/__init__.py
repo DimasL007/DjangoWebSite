6d6641510cd70ea16b9c50abb18d821e199e5215
DimasL007 <dimalinuk93@gmail.com>
2025-06-18 01:50:28 +0300

    Додано application

---
+++ application/migrations/__init__.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def home_view(request):
+    return render(request, 'home.html', {'title': 'Home'})
+
+def about_view(request):
+    return render(request, 'about.html', {'title': 'About'})
+
+def contact_view(request):
+    return render(request, 'contact.html', {'title': 'Contact'})
+
+def services_view(request):
+    return render(request, 'services.html', {'title': 'Services'})
+
+def gallery_view(request):
+    return render(request, 'gallery.html', {'title': 'Gallery'})
